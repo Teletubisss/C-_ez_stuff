ee5a3597fea22075f52585d879cd4a99e21e9de8
Teletubiss <natalagorny@gmail.com>
2025-04-30 10:09:45 +0200

    while loop

---
+++ BegginerProgramming5/whileLoop.cs
@@ -0,0 +1,20 @@
+﻿int count = 0;
+
+while (count < 10)
+{
+    Console.WriteLine("Number: " + count);  
+    count++;   //count = count + 1
+}
+Console.WriteLine("Count is complete!");
+
+
+
+int number = 0;
+Console.WriteLine("Please enter valid numbers below 100:");
+
+while (number < 100)
+{
+    number = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("The number you entered was too large, the loop has ended...");
