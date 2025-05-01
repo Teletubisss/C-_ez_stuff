ad91a6798ed3bb77316cdbfd4ad61004a208b7fe
Teletubiss <natalagorny@gmail.com>
2025-05-01 14:35:37 +0200

    no message

--- BegginerProgramming11/Hangman.cs
+++ BegginerProgramming11/Hangman.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Hangman
+{
+    void getValidWords()
+    {
+        var wordsObj = new Words();                   //nowa zmienna z klasa Words
+        string[] words = wordsObj.ListWords();            //nowa talbica words z lista words
+
+        var rand = new Random();                         //new Random() - obiekt ktory randomuje, rand - zmienna 
+        var randomWord = words[rand.Next(words.Length)];  //rand.Next losuje z zakresu 0 do length - 1
+
+        Console.WriteLine("Wylosowane słowo: " + randomWord);
+
+
+    }
+}
+
+
